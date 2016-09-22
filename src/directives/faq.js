7b9a6c7bd5ee8efb40d01b62849c936aedd438b0
Tres <nicolo.tresoldi@hotmail.it>
2016-09-22 14:45:22 +0200

    First two blocks of content

--- src/directives/faq.js
+++ src/directives/faq.js
@@ -6,7 +6,11 @@
         'title': 'paneTitle',
         'body': 'paneBody'
       },
-      template: '<div class="title" ng-transclude="title" ng-click="showContent =! showContent">Fallback Title</div>' +
-                  '<div ng-transclude="body" ng-show="showContent"></div>'
+      template: '<div class="row border">'+
+                '<div class="col-md-10 col-sm-10 col-xs-10 title" ng-transclude="title" ng-click="showContent =! showContent"></div>' + 
+                '<span class="col-md-2 col-sm-2 col-xs-2 text-right title-icon" ng-show="!showContent"><i class="fa fa-chevron-down"></i></span>' +
+                '<span class="col-md-2 col-sm-2 col-xs-2 text-right title-icon" ng-show="showContent"><i class="fa fa-chevron-up"></i></span>' +
+                '<div class="col-md-12" ng-transclude="body" ng-show="showContent"></div>' + 
+                '</div>'
     };
 })
