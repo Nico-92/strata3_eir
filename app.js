2aaa5aca3bcfe0391f9bd8488880ae292c679f99
Tres <nicolo.tresoldi@hotmail.it>
2016-09-27 12:01:47 +0200

    src files for heroku

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -13,18 +11,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /src
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src')));
-
-var api = [];
-api.push(require('./routes/api/login'));
-api.push(require('./routes/api/client'));
-app.use('/api/', api);
 
 app.use('/', index);
 
